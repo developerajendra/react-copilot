91daf61ecaf6bff51363cf4b1c9d8aa9a5371d50
rn2127 <rajendra.neupane@maxlifeinsurance.com>
2023-12-10 12:06:25 +0530

    add: init

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/App.scss';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
